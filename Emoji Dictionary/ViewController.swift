099d6cbba26d01fcbef51c69c0bb3348b2b712c2
Mickey Sonneveld <mickeysonneveld@Mickeys-MacBook-Pro.local>
2016-11-10 16:32:29 +0100

    Different emoji’s per row

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet weak var tableView: UITableView!
     
+    var emojis = ["😂","😎","😁","😡","😇","😱","👽", "👹","💪🏿"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,11 +23,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emojis.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
         let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
     
